18a969c4ddd43fc0dce820885146eac8a6117dbd
MaHdIn2020 <154902928+MaHdIn2020@users.noreply.github.com>
2024-09-01 18:15:31 +0600

    Update login.php

--- login.php
+++ login.php
@@ -1,22 +1,30 @@
-<?php
-    include('connection.php');
-    if (isset($_POST['submit'])) {
-        $username = $_POST['user'];
-        $password = $_POST['pass'];
-
-        $sql = "select * from login where username = '$username' and password = '$password'";  
-        $result = mysqli_query($conn, $sql);  
-        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
-        $count = mysqli_num_rows($result);  
-        
-        if($count == 1){  
-            header("Location: welcom.php");
-        }  
-        else{  
-            echo  '<script>
-                        window.location.href = "index.php";
-                        alert("Login failed. Invalid username or password!!")
-                    </script>';
-        }     
-    }
-    ?>
+<?php include('config.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Registration system PHP and MySQL</title>
+  <link rel="stylesheet" type="text/css" href="style1.css">
+</head>
+<body>
+  <div class="header">
+  	<h2>Login</h2>
+  </div>
+  <form method="post" action="login.php">
+  	<?php include('errors.php'); ?>
+  	<div class="input-group">
+  		<label>Username</label>
+  		<input type="text" name="username" >
+  	</div>
+  	<div class="input-group">
+  		<label>Password</label>
+  		<input type="password" name="password">
+  	</div>
+  	<div class="input-group">
+  		<button type="submit" class="btn" name="login_user">Login</button>
+  	</div>
+  	<p>
+  		Not yet a member? <a href="register.php">Sign up</a>
+  	</p>
+  </form>
+</body>
+</html>
