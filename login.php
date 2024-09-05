15404058fa67bb2ddf37b2913390ffd5b042e547
MaHdIn2020 <154902928+MaHdIn2020@users.noreply.github.com>
2024-09-05 22:48:09 +0600

    Update login.php

--- login.php
+++ login.php
@@ -1,105 +1,36 @@
-<?php include('config.php') ?>
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Registration system PHP and MySQL</title>
-   <style>
-	
-	* {
-	margin: 0px;
-	padding: 0px;
-	}
-	body {
-	font-size: 120%;
-	background: #F8F8FF;
-	background: url('travel_login.jpg') no-repeat;
-	background-size: 1920px 1080px;
-	background-attachment: cover;
-	
-}
-	
+<?php
+$host = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbname = "project";
 
-	.header {
-	width: 30%;
-	margin: 50px auto 0px;
-	color: white;
-	background: #8d0150;
-	text-align: center;
-	border: 1px solid #B0C4DE;
-	border-bottom: none;
-	border-radius: 10px 10px 0px 0px;
-	padding: 20px;
-	}
-	form, .content {
-	width: 30%;
-	margin: 0px auto;
-	padding: 20px;
-	border: 1px solid #B0C4DE;
-	background: white;
-	border-radius: 0px 0px 10px 10px;
-	}
-	.input-group {
-	margin: 10px 0px 10px 0px;
-	}
-	.input-group label {
-	display: block;
-	text-align: left;
-	margin: 3px;
-	}
-	.input-group input {
-	height: 30px;
-	width: 93%;
-	padding: 5px 10px;
-	font-size: 16px;
-	border-radius: 5px;
-	border: 1px solid gray;
-	}
-	.btn {
-	padding: 10px;
-	font-size: 15px;
-	color: white;
-	background: #8d0150;
-	border: none;
-	border-radius: 5px;
-	}
-	.error {
-	width: 92%; 
-	margin: 0px auto; 
-	padding: 10px; 
-	border: 1px solid #a94442; 
-	color: #a94442; 
-	background: #f2dede; 
-	border-radius: 5px; 
-	text-align: left;
-	}
-	.success {
-	color: #3c763d; 
-	background: #dff0d8; 
-	border: 1px solid #3c763d;
-	margin-bottom: 20px;
-	}
-   </style>
-</head>
-<body>
-  <div class="header">
-  	<h2>Login</h2>
-  </div>
-  <form method="post" action="home.php">
-  	<?php include('errors.php'); ?>
-  	<div class="input-group">
-  		<label>Username</label>
-  		<input type="text" name="username" >
-  	</div>
-  	<div class="input-group">
-  		<label>Password</label>
-  		<input type="password" name="password">
-  	</div>
-  	<div class="input-group">
-  		<button type="submit" class="btn" name="login_user">Login</button>
-  	</div>
-  	<p>
-  		Not yet a member? <a href="register.php">Sign up</a>
-  	</p>
-  </form>
-</body>
-</html>
+// Create connection
+$conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $conn->real_escape_string($_POST['username']);
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM users WHERE username='$username'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        if (password_verify($password, $row['password'])) {
+            // Redirect to home page after successful login
+            header("Location: home.php");
+            exit();
+        } else {
+            echo "Invalid password.";
+        }
+    } else {
+        echo "No user found with that username.";
+    }
+}
+
+$conn->close();
+?>
