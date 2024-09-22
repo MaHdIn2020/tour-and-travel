6894dbea293df033d8ed498daf79dad7ca8e242c
MaHdIn2020 <154902928+MaHdIn2020@users.noreply.github.com>
2024-09-22 13:14:59 +0600

    Update db.php

--- project/db.php
+++ project/db.php
@@ -1,13 +1,13 @@
 <?php
-$servername = "localhost";  // Change this if you're using a different host
-$username = "root";         // Update with your DB username
-$password = "";             // Update with your DB password
-$dbname = "project";        // Your database name
+$servername = "localhost";  
+$username = "root";         
+$password = "";             
+$dbname = "project";        
 
-// Create connection
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
